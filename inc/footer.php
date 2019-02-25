f88cddc5a263f9a2cd771f61ebb9737cfb327d31
rodrigoAMF <rodrigoamf@outlook.com>
2019-02-25 16:47:10 -0300

    Add estrutura básica dos arquivos

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+    <!-- jQuery -->
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
+
+    <!-- Bootstrap -->
+    <!--<script src="js/bootstrap.min.js"></script>-->
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("inc/header.php");
+?>
+
+    Body content
+
+<?php
+    require_once("inc/footer.php");
+?>
