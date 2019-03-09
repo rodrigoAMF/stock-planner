4546575b79c1340543c67f0fb371cd86e5bce05b
rodrigoAMF <rodrigoamf@outlook.com>
2019-03-09 01:40:41 -0300

    Update na estrutura básica dos arquivos
    Adicionado arquivos do bootstrap e font awesome, ambos testados e funcionando.
    
    Excluído header temporariamente.

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,11 @@
-    <!-- jQuery -->
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
+    <!-- A ordem de importação importa!, jQuery primeiro, depois Popper.js e por fim Bootstrap JS -->
+    <!-- jQuery 3.3.1 Slim -->
+    <script src="js/jquery.slim.min.js"></script>
+
+    <!-- Popper.js -->
+    <script src="js/popper.min.js"></script>
 
     <!-- Bootstrap -->
-    <!--<script src="js/bootstrap.min.js"></script>-->
-
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,8 +1,66 @@
-<?php
-    require_once("inc/header.php");
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    Body content
+    <title>Stock Planner - Home</title>
+    <!-- Ícone do site -->
+    <link rel="icon" href="img/favicon.png">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <!-- Ícones do font awesome 5.7.2 -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- CSS Base para todas as páginas -->
+    <link rel="stylesheet" href="css/base.css">
+</head>
+
+<body>
+    <div class="container">
+        <!-- Criar as linhas e colunas de acordo com necessidade -->
+    </div>
+
+
+    <!--
+        Abaixo segue código para testar bootstrap e font awesome.
+        Ele pode ser apagado antes de iniciar o desenvolvimento
+    -->
+
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      Launch demo modal
+    </button>
+    <br>
+    <i class="fas fa-igloo fa-xs"></i>
+    <i class="fas fa-igloo fa-sm"></i>
+    <i class="fas fa-igloo fa-lg"></i>
+    <i class="fas fa-igloo fa-2x"></i>
+    <i class="fas fa-igloo fa-3x"></i>
+    <i class="fas fa-igloo fa-5x"></i>
+    <i class="fas fa-igloo fa-7x"></i>
+    <i class="fas fa-igloo fa-10x"></i>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Apagar até aqui -->
 
 <?php
     require_once("inc/footer.php");
