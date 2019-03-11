9b21d46a229db9af35a08d5383d2b9f677456954
rodrigoAMF <rodrigoamf@outlook.com>
2019-03-11 14:58:25 -0300

    Removido códigos de teste da página inicial

--- index.php
+++ index.php
@@ -8,46 +8,6 @@
         <!-- Criar as linhas e colunas de acordo com necessidade -->
     </div>
 
-    <!--
-        Abaixo segue código para testar bootstrap e font awesome.
-        Ele pode ser apagado antes de iniciar o desenvolvimento
-    -->
-    <!--
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-      Launch demo modal
-    </button>
-    <br>
-    <i class="fas fa-igloo fa-xs"></i>
-    <i class="fas fa-igloo fa-sm"></i>
-    <i class="fas fa-igloo fa-lg"></i>
-    <i class="fas fa-igloo fa-2x"></i>
-    <i class="fas fa-igloo fa-3x"></i>
-    <i class="fas fa-igloo fa-5x"></i>
-    <i class="fas fa-igloo fa-7x"></i>
-    <i class="fas fa-igloo fa-10x"></i>
-
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            ...
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Apagar até aqui -->
-
 <?php
-    //require_once("inc/footer.php");
+    require_once("inc/footer.php");
 ?>
