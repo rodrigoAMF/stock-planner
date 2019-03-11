67393c086f1c6c7a265a0c51a5f48c7c6b3395e2
rodrigoAMF <rodrigoamf@outlook.com>
2019-03-11 15:12:55 -0300

    Add arquivo com constantes e credencias para acesso ao bd

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* O nome do banco de dados*/
+define('DB_NAME', 'stock_planner');
+/* Usuário do banco de dados MySQL */
+define('DB_USER', 'root');
+/* Senha do banco de dados MySQL */
+define('DB_PASSWORD', '');
+/* nome do host do MySQL */
+define('DB_HOST', 'localhost');
+
+/* caminho do arquivo de banco de dados */
+define('DBAPI', 'inc/database.php');
+
+/* caminhos dos templates de header e footer */
+define('HEADER_TEMPLATE', 'inc/header.php');
+define('FOOTER_TEMPLATE', 'inc/footer.php');
