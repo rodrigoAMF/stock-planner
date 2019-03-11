3f59202048054bd0ce4ffa4de4a51c795de32c4c
rodrigoAMF <rodrigoamf@outlook.com>
2019-03-11 15:15:19 -0300

    Add arquivo para acesso ao BD

---
+++ inc/database.php
@@ -0,0 +1,22 @@
+<?php
+
+mysqli_report(MYSQLI_REPORT_STRICT);
+
+function open_database() {
+	try {
+		$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		mysqli_set_charset($conn,"utf8");
+		return $conn;
+	} catch (Exception $e) {
+		echo $e->getMessage();
+		return null;
+	}
+}
+
+function close_database($conn) {
+	try {
+		mysqli_close($conn);
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+}
