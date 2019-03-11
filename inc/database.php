95c36907f8bcd69d38535797c37469812b64eb18
rodrigoAMF <rodrigoamf@outlook.com>
2019-03-11 15:27:45 -0300

    Movidas funções de "funcoes_bd.php" para "database.php
    Removido arquivo "funcoes_bd.php"

--- inc/database.php
+++ inc/database.php
@@ -20,3 +20,18 @@ function close_database($conn) {
 		echo $e->getMessage();
 	}
 }
+
+// Retorna 1 produto do banco de dados a partir do seu ID
+function getProdutoPorId($id){
+    $conexao = open_database();
+
+    $query = "SELECT * FROM produtos where id = " . $id;
+
+    $resultado = $conexao->query($query);
+
+    $dados = $resultado->fetch_all(MYSQLI_ASSOC);
+
+    close_database($conexao);
+
+    return $dados;
+}
