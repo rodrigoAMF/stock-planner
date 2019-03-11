fd1b7aa0d36bb84a2b497696ab52375e371c3119
gabii6431 <gabii6431@gmail.com>
2019-03-11 17:06:43 -0300

    Add página cadastro-produto e css

---
+++ cadastro-produto.php
@@ -0,0 +1,13 @@
+<?php
+    require_once("funcoes.php");
+
+    incluiCabecalho("Stock Planner - Home", "cadastro-produto");
+?>
+<body>
+    <div class="container">
+        <!-- Criar as linhas e colunas de acordo com necessidade -->
+    </div>
+
+<?php
+    require_once(FOOTER_TEMPLATE);
+?>
