000487741d55a5b77c3cb8251e9108159dd12e3e
rodrigoAMF <rodrigoamf@outlook.com>
2019-03-14 17:01:24 -0300

    Update na versão do jquery, trocado slim para "normal"
    Slim não oferece suporte ao $.ajax e por conta disso precisou ser trocado

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
     <!-- A ordem de importação importa!, jQuery primeiro, depois Popper.js e por fim Bootstrap JS -->
     <!-- jQuery 3.3.1 Slim -->
-    <script src="js/jquery.slim.min.js"></script>
+    <script src="js/jquery.min.js"></script>
 
     <!-- Popper.js -->
     <script src="js/popper.min.js"></script>
