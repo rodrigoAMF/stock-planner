0773de4dc0b530cea765643c1c0943a1a7d64a0c
gabii6431 <gabii6431@gmail.com>
2019-03-18 15:24:01 -0300

    Update lista-produtos.php

--- lista-produtos.php
+++ lista-produtos.php
@@ -7,7 +7,12 @@
 
 <div class="container">
     <!-- Criar as linhas e colunas de acordo com necessidade -->
-
+    <select class="custom-select custom-select-sm">
+        <option value="20">20%</option>
+        <option value="50">50%</option>
+        <option value="80">80%</option>
+    </select>
+    <button type="button" class="btn btn-secondary" id="confirma_porcentagem">Confirmar</button>
     <table class="table">
         <thead>
             <tr>
@@ -26,6 +31,27 @@
 
     <?php
         $produtos = getTodosProdutos();
+
+        for ($i=0; $i < sizeof($produtos); $i++) { 
+            $produtos[$i]['porcentagem'] = floatval($produtos[$i]['quantidade']/$produtos[$i]['estoque_ideal']);
+        }
+
+        
+        
+        for($i = 0; $i < sizeof($produtos); $i++)
+        {
+            $menorPorcentagem = 100000;
+            for ($j = $i; $j < sizeof($produtos) ; $j++) { 
+                if($produtos[$j]['porcentagem'] < $menorPorcentagem){
+                    $menorPorcentagem = $produtos[$j]['porcentagem'];
+                    $posicao_menor_porcentagem = $j;
+                }
+            }
+            $aux =  $produtos[$posicao_menor_porcentagem];
+            $produtos[$posicao_menor_porcentagem] = $produtos[$i];
+            $produtos[$i] = $aux;         
+        }
+
         foreach ($produtos as $produto) {
             echo "<tr>";
             echo "<td>" . $produto['id'] . "</td>";
