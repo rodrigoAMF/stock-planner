8ad96d3e762f128b49317014e6221e812960e444
rodrigoAMF <rodrigoamf@outlook.com>
2019-03-28 15:54:16 -0300

    Finalizado função ordenar por click no header da tabela
    Apenas as colunas nome, quantidade e estado crítico são clicáveis.

--- js/lista-produtos.js
+++ js/lista-produtos.js
@@ -1,3 +1,7 @@
+let clickNome = false;
+let clickQuantidade = false;
+let clickEstadoCritico = false;
+
 $(".ordenavel").on("click", function(event)
 {
 	let nomeCampo = $(this).text();
@@ -8,14 +12,83 @@ $(".ordenavel").on("click", function(event)
 	}
 
 	if(nomeCampo == "NOME"){
+		let url = "get-produto.php?busca=&filtro=&parametroOrdenacao=";
+		if(clickNome == false){
+			url += 1;
+		}else{
+			url += -1;
+		}
 
+		clickNome = !clickNome;
+
+		var request = $.ajax({
+			url: url,
+			cache: false
+		});
+
+		request.done(function(msg) {
+			$('table tbody').remove();
+			$('table').append("<tbody>");
+			$('table tbody').append(msg);
+			$('table').append("</tbody>");
+		});
+
+		request.fail(function(jqXHR, textStatus) {
+			alertify.error('Falha ao ordenar produtos');
+		});
 	}
 	if(nomeCampo == "QUANTIDADE"){
+		let url = "get-produto.php?busca=&filtro=&parametroOrdenacao=";
+		if(clickQuantidade == false){
+			url += 7;
+		}else{
+			url += -7;
+		}
 
+		clickQuantidade = !clickQuantidade;
+
+		var request = $.ajax({
+			url: url,
+			cache: false
+		});
+
+		request.done(function(msg) {
+			$('table tbody').remove();
+			$('table').append("<tbody>");
+			$('table tbody').append(msg);
+			$('table').append("</tbody>");
+		});
+
+		request.fail(function(jqXHR, textStatus) {
+			alertify.error('Falha ao ordenar produtos');
+		});
 	}
 	if(nomeCampo == "")
 	{
+		let url = "get-produto.php?busca=&filtro=&parametroOrdenacao=";
+		if(clickEstadoCritico == false){
+			url += 8;
+		}else{
+			url += -8;
+		}
 
+		clickEstadoCritico = !clickEstadoCritico;
+
+		var request = $.ajax({
+			url: url,
+			cache: false
+		});
+
+		request.done(function(msg) {
+			$('table tbody').remove();
+			$('table').append("<tbody>");
+			$('table tbody').append(msg);
+			$('table').append("</tbody>");
+		});
+
+		request.fail(function(jqXHR, textStatus) {
+			alertify.error('Falha ao ordenar produtos');
+		});
 	}
 
 });
