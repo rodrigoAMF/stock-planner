24604de5d3a6f3bbbb5f6b2d12b8f1dbb82e953f
rodrigoAMF <rodrigoamf@outlook.com>
2019-04-29 16:55:22 -0300

    Fix no importar-arquivo
    Página finalizada.
    Co-authored-by: Nadia Nogues <nadianogues19@gmail.com>

--- processa-arquivo.php
+++ processa-arquivo.php
@@ -1,109 +1,131 @@
 <?php
 session_start();
-  require_once("model/Produto.php");
-  require_once("controller/ProdutoController.php");
-  require_once("controller/CategoriaController.php");
+require_once("model/Produto.php");
+require_once("controller/ProdutoController.php");
+require_once("controller/CategoriaController.php");
 
 
-  $arquivo_temp = $_FILES['arquivo']['tmp_name'];
-  $dadosLidos = file($arquivo_temp);
-  $cont = 0;
-  $erro = false;
+$arquivo_temp = $_FILES['arquivo']['tmp_name'];
+$dadosLidos = file($arquivo_temp);
+$cont = 0;
+$erro = false;
 
-  for($i = 0; $i < sizeof($dadosLidos); $i++)
-  {
-      $mensagemErro = " ";
-      $produtos[$i] = new Produto();
-      $existeCategoria = false;
-      $dadosLidos[$i] = trim($dadosLidos[$i]);
-      $camposProduto = explode("\t", $dadosLidos[$i]);
-      
+$produtos = Array();
 
-  
+for($i = 0; $i < sizeof($dadosLidos) && !$erro; $i++)
+{
+    $mensagemErro = "";
+    $produtos[$i] = new Produto();
+
+    $existeCategoria = false;
+    $dadosLidos[$i] = trim($dadosLidos[$i]);
+    $camposProduto = explode("\t", $dadosLidos[$i]);
+
+    foreach($camposProduto as $campo){
+        if($campo == ""){
+            $mensagemErro = "Campo com valor vazio na linha " . strval($i+1);
+            $erro = true;
+        }
+    }
+
+    // Verifica se usuário informou os 8 campos obrigatórios
+    echo sizeof($camposProduto) . "<br>";
     if (sizeof($camposProduto)<8) {
-      $mensagemErro = "Algum campo não preenchido na linha " . ($i+1);
-      $erro = true;
-      $i=sizeof($dadosLidos);
+        $mensagemErro = "Existe algum campo não preenchido na linha " . strval($i+1);
+        $erro = true;
     }
 
     $categoriaController = CategoriaController::getInstance();
     $categorias = $categoriaController->getCategorias();
 
-    for ($i=0; $i < sizeof($categorias); $i++) {
+    // Verifica se já existe uma categoria com o nome atual
+    for ($j=0; $j < sizeof($categorias); $j++) {
 
-      if ($categorias[$i]['nome'] == $camposProduto[6]) {
-          $existeCategoria = true;
-          break;
-      }
+        if ($categorias[$j]['nome'] == $camposProduto[6]) {
+            $existeCategoria = true;
+            break;
+        }
     }
+    // Se não existir categoria, cria uma
     if (!$existeCategoria) {
-      $categoria = new Categoria();
-      $categoria->setNomeNovo($camposProduto[6]);
-      $categoriaController->cadastraCategoria($categoria);
-      
+        $categoria = new Categoria();
+        $categoria->setNomeNovo($camposProduto[6]);
+        $categoriaController->cadastraCategoria($categoria);
     }
 
-    
-
-    if (!ehNumerico($camposProduto[2])) {
-      $mensagemErro = "O campo catmat no produto " . strval($i) . " deve ser numérico A";
-      $erro = true;
-      $i=sizeof($dadosLidos);
+    if (strlen($camposProduto[0]) > 100) {
+        $mensagemErro = "Erro na linha " . strval($i+1) . ". O campo 'Nome' suporta no máximo 100 caracteres";
+        $erro = true;
     }
 
-    if (!ehNumerico($camposProduto[4])) {
-      $mensagemErro = "O campo estoque ideal no produto " . strval($i) . " deve ser numérico B";
-      $erro = true;
-      $i=sizeof($dadosLidos);
+    // Verifica se os campos são numéricos
+    if (!is_numeric($camposProduto[2])) {
+        $mensagemErro = "Erro na linha " . strval($i+1) . ". O campo 'Catmat' deve ser numérico";
+        $erro = true;
     }
 
-    if (!ehNumerico($camposProduto[3])){
-      $mensagemErro = "O campo quantidade no produto " . strval($i) . " deve ser numérico C";
-      $erro = true;
-      $i=sizeof($dadosLidos);
+    if(strlen($camposProduto[2]) > 6){
+        $mensagemErro = "Erro na linha " . strval($i+1) . ". O campo 'Catmat' deve ter no máximo 6 digitos";
+        $erro = true;
+    }
+
+    if (!is_numeric($camposProduto[3])){
+        $mensagemErro = "Erro na linha " . strval($i+1) . ". O campo 'Quantidade' deve ser numérico";
+        $erro = true;
+    }
+
+    if(strlen($camposProduto[3]) > 6){
+        $mensagemErro = "Erro na linha " . strval($i+1) . ". O campo 'Quantidade' deve ter no máximo 6 digitos";
+        $erro = true;
+    }
+
+    if (!is_numeric($camposProduto[4])){
+        $mensagemErro = "Erro na linha " . strval($i+1) . ". O campo 'Estoque Ideal' deve ser numérico";
+        $erro = true;
     }
 
     if (strlen($camposProduto[5]) > 3) {
-      $mensagemErro = "O campo posição no produto " . strval($i) . " suporta apenas 3 caracteres";
-      $erro = true;
-      $i=sizeof($dadosLidos);
+        $mensagemErro = "Erro na linha " . strval($i+1) . ". O campo posição suporta apenas 3 caracteres";
+        $erro = true;
     }
 
     if(!$erro){
-      $produtos[$i]->setNome($camposProduto[0]);
-      $produtos[$i]->setIdentificacao($camposProduto[1]);
-      $produtos[$i]->setCatmat($camposProduto[2]);
-      $produtos[$i]->setQuantidade($camposProduto[3]);
-      $produtos[$i]->setEstoqueIdeal($camposProduto[4]);
-      $produtos[$i]->setPosicao($camposProduto[5]);
-      $produtos[$i]->getCategoria()->setNome($camposProduto[6]);
-      $produtos[$i]->setDescricao($camposProduto[7]);
+        $produtos[$i]->setNome($camposProduto[0]);
+        $produtos[$i]->setIdentificacao($camposProduto[1]);
+        $produtos[$i]->setCatmat($camposProduto[2]);
+        $produtos[$i]->setQuantidade($camposProduto[3]);
+        $produtos[$i]->setEstoqueIdeal($camposProduto[4]);
+        $produtos[$i]->setPosicao($camposProduto[5]);
+        $produtos[$i]->getCategoria()->setNome($camposProduto[6]);
+        $produtos[$i]->setDescricao($camposProduto[7]);
     }
-    
-  }
+}
 
-
-
-  if (!$erro) {
+// Se não existe nenhum erro no arquivo
+if (!$erro) {
+    // Cadastra todos os produtos
+    $produtoDuplicado = "";
+    $linhaDuplicado = 1;
     foreach ($produtos as $produto) {
         $produtoController = ProdutoController::getInstance();
-        $resultadoCadastro = $produtoController->cadastraProduto($produto);
-        //echo $resultadoCadastro;
+        $resultadoCadastro = $produtoController->cadastraProduto($produto, "1S2019");
+        if($resultadoCadastro == -1){
+            $erro = true;
+            $produtoDuplicado = $produto->getNome();
+            break;
+        }
+        $linhaDuplicado++;
     }
-  }
+    if(!$erro){
+        $_SESSION['msg'] = "<p> Produtos foram cadastrados com sucesso</p>";
+    }else{
+        $_SESSION['msg'] = "<p> Produto '" . $produtoDuplicado . "' duplicado na linha " . $linhaDuplicado . "</p>";
+    }
 
-  if ($erro) {
+}else {
     // significa que tem erro
     $_SESSION['msg'] = "<p>" .$mensagemErro . "</p>";
-  } else{
-    echo "cadastrado";
-    $_SESSION['msg'] = "<p> Todos os produtos foram cadastrados com sucesso</p>";
-  }
-  //header("Location: importar-produtos.php");
-
-  function ehNumerico($campo): bool{
-
-    return is_numeric($campo);
-  }
+}
+header("Location: importar-produtos.php");
 
 ?>
