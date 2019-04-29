e500fa078b421a3f0f3dad9cdd834f14b1dd5236
rodrigoAMF <rodrigoamf@outlook.com>
2019-04-29 17:33:12 -0300

    Fix na responsividade da página importar produtos

--- importar-produtos.php
+++ importar-produtos.php
@@ -9,36 +9,41 @@
 
 <div class="container">
   <div id="principal">
-    <form method="post" action="processa-arquivo.php" enctype="multipart/form-data" class="was-validated">
-      <div class="form-row">
-        <div class="form-group col-xl-12 col-md-8 col-sm-4 col-3 col-lg-10 texto">
-        <h4><strong> ATENÇÃO! </strong></h4>
-          <p>  
-            Os produtos do arquivo devem estar na seguinte ordem e separados por TABULAÇÃO:<br>
-            <i>Nome, identificação, catmat, quantidade, estoqueIdeal, posição, categoria e descrição.</i>
-          </p>
-        </div>
-
-        <div class="form-group col-xl-12 col-md-8 col-sm-4 col-3 col-lg-10 caixaImportar">
-            <div class="custom-file">
-
-              <label class="custom-file-label" for="validatedCustomFile"><span id="label-nomeArquivo">Nenhum arquivo selecionado</span></label>
-              <input type='file' class="custom-file-input" id="file" name="arquivo" accept=".txt"  required>
-            </div>
-            <div>
-              <input class="btn btn-primary botaoImportar" type="submit" value="Importar">
-            </div>
-        </div>
+      <div class="row">
+          <div class="col-md-12">
+              <h4><strong> ATENÇÃO! </strong></h4>
+              <p>
+                  Os produtos do arquivo devem estar na seguinte ordem e separados por TABULAÇÃO:<br>
+                  <i>Nome, identificação, catmat, quantidade, estoqueIdeal, posição, categoria e descrição.</i>
+              </p>
+          </div>
       </div>
-    </form>
-    <?php
-    if (isset($_SESSION['msg'])) {
-      echo $_SESSION['msg'];
-      unset($_SESSION['msg']);
-    }
-
-    ?>
-  </div>
+      <div class="row">
+          <div class="col-md-12">
+              <form method="post" action="processa-arquivo.php" enctype="multipart/form-data" class="was-validated">
+                  <div class="form-group">
+                      <label class="custom-file-label" for="validatedCustomFile"><span id="label-nomeArquivo">Nenhum arquivo selecionado</span></label>
+                      <input type='file' class="custom-file-input form-control-file" id="file" name="arquivo" accept=".txt"  required>
+                  </div>
+                  <div class="form-group">
+                      <div class="centraliza">
+                          <input class="btn btn-primary botaoImportar" type="submit" value="Importar Produtos">
+                      </div>
+                  </div>
+              </form>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-12">
+              <?php
+                  if (isset($_SESSION['msg'])) {
+                      echo $_SESSION['msg'];
+                      unset($_SESSION['msg']);
+                  }
+              ?>
+          </div>
+      </div>
+    </div>
 </div>
 
 <?php
